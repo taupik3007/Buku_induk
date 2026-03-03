350c606759dac264a0064aa083ac6f9be8688bca
taupik3007 <taupikpathurrohman@gmail.com>
2026-03-03 22:49:50 +0700

    administration sidebar fix

--- resources/views/administration/head.blade.php
+++ resources/views/administration/head.blade.php
@@ -9,6 +9,7 @@
   
     <!-- Core Css -->
     <link rel="stylesheet" href="{{asset('assets/css/styles.css')}}" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tabler-icons/3.35.0/tabler-icons.min.css" integrity="sha512-gzw5zNP2TRq+DKyAqZfDclaTG4dOrGJrwob2Fc8xwcJPDPVij0HowLIMZ8c1NefFM0OZZYUUUNoPfcoI5jqudw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   
     <title>@yield('title')</title>
   
