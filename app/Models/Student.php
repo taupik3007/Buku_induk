a40b17d52c82bbe7e635e328715d58c8d8b4e0d7
taupik3007 <taupikpathurrohman@gmail.com>
2026-04-11 23:12:23 +0700

    fix: student detail in ppdb reception

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -33,4 +33,8 @@ class Student extends Model
     {
         return $this->hasOne(PhysicalCondition::class, 'phy_student_id', 'std_id');
     }
+    public function previousEducation()
+    {
+        return $this->hasOne(Previous_Education::class, 'prv_student_id', 'std_id');
+    }
 }
