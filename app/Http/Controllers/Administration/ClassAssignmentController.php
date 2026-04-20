d6f2df4391f2acab570e8f459094fb8d89c712b0
taupik3007 <taupikpathurrohman@gmail.com>
2026-04-20 23:08:18 +0700

    continue classAssignment

--- app/Http/Controllers/Administration/ClassAssignmentController.php
+++ app/Http/Controllers/Administration/ClassAssignmentController.php
@@ -6,17 +6,28 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\PpdbSubmission;
 use App\Models\Majors;
+use App\Models\Ppdb;
+
 
 class ClassAssignmentController extends Controller
 {
-    public function index(){
-        $majorCount = Majors::count();
-        // Controller
-        $major = Majors::withCount([
-            'ppdbSubmissions as accepted_students_count' => fn($q) => $q->where('ppsu_status', 1)
-        ])->get();
-        // dd(Majors::with('ppdbSubmissions')->first());
-        $studentCount = PpdbSubmission::where('ppsu_status',1)->count();
-        return view('administration.class-assignment.index',compact('studentCount','majorCount','major'));
+    public function index(Request $request){
+         $ppdbList = Ppdb::orderByDesc('ppd_id')->get();
+    
+    // Default: PPDB aktif atau yang pertama
+    $selectedPpdb = $request->ppd_id 
+        ? Ppdb::find($request->ppd_id) 
+        : $ppdbList->first();
+    // $studentCount = PpdbSubmission::where('ppsu_status',1)
+    $major = Majors::withCount([
+        'ppdbSubmissions as accepted_students_count' => fn($q) => $q
+            ->where('ppsu_status', 1)
+            ->where('ppsu_ppdb_id', $selectedPpdb->ppd_id)
+    ])->get();
+    // $major = Majors::with('ppdbSubmissions')->get();
+    // dd($major);
+    $studentCount =PpdbSubmission::where('ppsu_ppdb_id',$selectedPpdb->ppd_id)->where('ppsu_status',1)->count();
+
+    return view('administration.class-assignment.index', compact('major', 'ppdbList', 'selectedPpdb','studentCount'));
     }
 }
